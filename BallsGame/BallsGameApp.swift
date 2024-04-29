2419fd346d5f823b31af5538d23546a71feba913
Anton <danilovanton724@gmail.com>
2024-04-29 11:29:51 +0200

    Initial Commit

---
+++ BallsGame/BallsGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  BallsGameApp.swift
+//  BallsGame
+//
+//  Created by Anton on 29/4/24.
+//
+
+import SwiftUI
+
+@main
+struct BallsGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BallsGame/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  BallsGame
+//
+//  Created by Anton on 29/4/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
